ee1e47246c0171a7afafa07c1abb974e52bdb2c5
acgawade <gawade.achyut96@gmail.com>
2024-10-05 21:25:39 +0100

    Testing sonar phase

--- src/frontend/src/pages/Home.jsx
+++ src/frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <h1>Welcome to Website Home Page, This is for Thesis </h1>;
+  return <h1>Welcome to Website Home Page, This is for Thesis by Shylaja </h1>;
 };
 
 export default Home;
