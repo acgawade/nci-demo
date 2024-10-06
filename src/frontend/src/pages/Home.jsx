be769f2a7b0fa1ff853933c6e813a6e0b945f42f
acgawade <gawade.achyut96@gmail.com>
2024-10-06 01:55:36 +0100

    Testing Trivy phase

--- src/frontend/src/pages/Home.jsx
+++ src/frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <h1>Welcome to Website Home Page, This is for Thesis by Shylaja </h1>;
+  return <h1>Welcome to Website Home Page, This is for NCI Thesis Cloud Computing </h1>;
 };
 
 export default Home;
