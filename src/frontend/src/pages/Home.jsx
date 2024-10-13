14c6de2af628e955336222942224634460791bc6
acgawade <gawade.achyut96@gmail.com>
2024-10-13 20:58:34 +0100

    Check for develop trigger

--- src/frontend/src/pages/Home.jsx
+++ src/frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <h1>Welcome to Website Home Page, This is for NCI Cloud Thesis Changes are almost done should trigger develop</h1>;
+  return <h1>Welcome to Website Home Page, This is for NCI Cloud should be working</h1>;
 };
 
 export default Home;
