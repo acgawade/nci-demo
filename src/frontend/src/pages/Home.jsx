aa8690f1f6abc21305996512abb2d050056eb0d4
acgawade <gawade.achyut96@gmail.com>
2024-10-13 17:17:06 +0100

    This is to check feature commit pipeline trigger

--- src/frontend/src/pages/Home.jsx
+++ src/frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <h1>Welcome to Website Home Page, This is for NCI CloudThesis  </h1>;
+  return <h1>Welcome to Website Home Page, This is for NCI CloudThesis  for test</h1>;
 };
 
 export default Home;
