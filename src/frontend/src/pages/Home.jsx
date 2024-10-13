270b53c0383734fadcac97a1e8d6fb4c9cece7ea
acgawade <gawade.achyut96@gmail.com>
2024-10-13 20:53:32 +0100

    Check for double pipeline execution

--- src/frontend/src/pages/Home.jsx
+++ src/frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <h1>Welcome to Website Home Page, This is for NCI Cloud Thesis Changes are almost done</h1>;
+  return <h1>Welcome to Website Home Page, This is for NCI Cloud Thesis Changes are almost done should trigger develop</h1>;
 };
 
 export default Home;
